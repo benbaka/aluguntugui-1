1afb7f6d1b2190a1df364d53a70a11c87791e08e
Benjamin Baka <baka@mpedigree.net>
2016-09-30 02:17:48 +0000

    Ben: Added auth routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  post 'auth/login'
+
+  get 'auth/logout'
+
+  get 'auth/home'
+
+  get 'auth/landing'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
