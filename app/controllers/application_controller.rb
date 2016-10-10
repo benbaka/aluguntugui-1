5fce89e9aa5cc6095a0d775440edcbe376ddf3d7
Benjamin Baka <baka@mpedigree.net>
2016-10-10 11:43:17 +0000

    Ben: overhead method implemented to check for authenticated users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :require_login
   protect_from_forgery with: :exception
+
+  include AuthHelper
+
+
+
+  private
+  def require_login
+    unless logged_in?
+      flash[:error] = "Please login."
+      redirect_to auth_home_url
+    end
+  end
 end
